41a3f7fd464c74a236566d251283c4312fe164d7
Elias Reutelsterz <elias@reutelsterz.info>
2023-11-19 12:00:26 +0100

    try1

--- frontend/enze_app/src/routes/+layout.svelte
+++ frontend/enze_app/src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import { base } from '$app/paths';
-	export const prerender = true;
 	export const trailingSlash = 'always';
 </script>
 
 <nav>
-	<a href="{base}/enze">Home</a>
-	<a href="{base}/enze/about">About</a>
+	<a href="https://eliasreutelsterz.github.io/enze">Home</a>
+	<a href="https://eliasreutelsterz.github.io/enze/about">About</a>
 </nav>
 
 <slot />
