0026f98e632c26373a8c9c6dbc6637bd3ac808db
Elias Reutelsterz <elias@reutelsterz.info>
2023-11-19 19:45:38 +0100

    switched to tailwind

--- README.md
+++ README.md
@@ -2,6 +2,3 @@
 Enzensperger needs some organization
 
 # How to deploy
-cd frontend/enze_app
-npm run build
-paste items in build folder to docs on top level
--- docs/_app/immutable/chunks/paths.a886a588.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_l6wl9a)==null?void 0:s.base)??"";var e;const a=((e=globalThis.__sveltekit_l6wl9a)==null?void 0:e.assets)??"https://eliasreutelsterz.github.io/enze";export{a,t as b};
